956fd4e6c7633351b9b65b3bd933874c4b42a236
alxgmpr <agompper@me.com>
2018-04-09 13:27:47 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'Bitrate',
+        script: './index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-54-91-150-70.compute-1.amazonaws.com',
+            key: '~/.ssh/bitrate_priv_key.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:alxgmpr/Bitrate.git',
+            path: '/home/ubuntu/Bitrate',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+};
