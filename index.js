aa447c7a752bc69a83148dd242b7ea0792bb3a55
alxgmpr <agompper@me.com>
2018-04-09 15:04:42 -0500

    Made things prettier

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('HEY!');
+app.get('/', function(req, res) {
+    res.send('Hell');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, function() {console.log('Server running on port 3000')});
