ad03ae2ff44789f47edf69be365f8885f1dbc453
Nathan Ortbals <nathan.ortbals@gmail.com>
2018-04-25 18:11:51 -0500

    changed "get data" query to include all days even if there is no data

--- sql-wrapper.js
+++ sql-wrapper.js
@@ -55,15 +55,24 @@ module.exports.GetAllDataBetween = function(currencyId, startDate, endDate) {
 
         const query = {
             sql:
-            'SELECT currency_id, DATE_FORMAT(date, \'%m-%d-%Y\') as date, price_us_dollars, ' +
-                'google_activity, twitter_mentions ' +
-            'FROM day_index ' +
-            'WHERE currency_id = ? ' +
-            'AND date >= ? ' +
-            'AND date <= ? '
+            'SELECT date_range.date, price_us_dollars, google_activity, twitter_mentions\n' +
+            'FROM ' +
+                '(Select date from' +
+                '(select adddate(\'1970-01-01\',t4.i*10000 + t3.i*1000 + t2.i*100 + t1.i*10 + t0.i) date from' +
+                '(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0, ' +
+                '(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1, ' +
+                '(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2, ' +
+                '(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3, ' +
+                '(select 0 i union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v ' +
+                'where date between ? and ?) as date_range ' +
+            'LEFT JOIN ' +
+                '(SELECT * ' +
+                'FROM day_index ' +
+                'WHERE currency_id = ?) as day_index ' +
+            'ON date_range.date = day_index.date '
         };
 
-        pool.query(query, [currencyId, startDate, endDate], function (err, results){
+        pool.query(query, [startDate, endDate, currencyId], function (err, results){
             if(err)
                 reject(err);
             else{
